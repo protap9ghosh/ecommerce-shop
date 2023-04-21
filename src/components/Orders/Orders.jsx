5755c2442f96cccf848a56daae6b5427963ebe9b
Protap Ghosh <protapg518@gmail.com>
2023-04-22 01:34:39 +0600

    add review item

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css"
 
 const Orders = () => {
     const cart = useLoaderData();
-    console.log(cart);
+    // console.log(cart);
 
     return (
         <div className="shop-container">
-            <div className="product-container">
-                <h2>Order Page: {cart.length}</h2>
+            <div className="review-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                    ></ReviewItem>)
+                }
             </div>
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
