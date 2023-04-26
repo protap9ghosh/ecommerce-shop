9f40a511d41465f75d2440caaa7574020996e20f
Protap Ghosh <protapg518@gmail.com>
2023-04-27 00:43:53 +0600

    setup firebase

---
+++ src/__Steps.js
@@ -0,0 +1,13 @@
+/*
+ * 1. Create firebase project 
+ * 2. Create web app
+ * 3. npm install firebase
+ * 4. Save firebase config and export default app
+ * 5. Build > Authentication > enable sing in method
+ * 6. 
+ * 7.
+ * 
+ * 
+ * 
+ * 
+ *  */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAzyhEb1F5Rut_OAOMhIgRUkZ2ztEICUxc",
+    authDomain: "e-commerce-shop-45c15.firebaseapp.com",
+    projectId: "e-commerce-shop-45c15",
+    storageBucket: "e-commerce-shop-45c15.appspot.com",
+    messagingSenderId: "381494092887",
+    appId: "1:381494092887:web:d4751700e676b4157f2f12",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
