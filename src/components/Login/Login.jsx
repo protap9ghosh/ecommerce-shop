5260e8742510863ec556cce87c90c79b8d7e1d38
Protap Ghosh <protapg518@gmail.com>
2023-04-27 11:52:03 +0600

    style login section

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,21 @@ import "./Login.css"
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming soon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div>
+                    <div className='form-control'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name='email' id='' required />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' id='' required />
+                    </div>
+                    <input className='btn-submit' type="submit" value="Login" />
+                </div>
+            </form>
         </div>
     );
 };
