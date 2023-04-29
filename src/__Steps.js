a94fe657e6a77b48a0b97553b3c9fa0523b6643c
Protap Ghosh <protapg518@gmail.com>
2023-04-29 11:04:20 +0600

    firebase init

--- src/__Steps.js
+++ src/__Steps.js
@@ -19,6 +19,29 @@
  * 4. Set context Value
  * 5. Set the provider
  * 
+ *  */
+
+
+/* 
+ TODO Firebase Hosting Steps
+ ?    One time per PC
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
  * 
+ ?    For each project on time
+ * 1. firebase init
+ * 2. proceed
+ * 3. hosting: firebase ( up and down arrow ) use space bar to select
+ * 4. existing project 
+ * 5. select the project careful
+ * 6. Which project as public directory: vite=dist react=build
+ * 7. Single page application: Yes
+ * 8. Continuous deployment: No
  * 
+ ?    For every time deploy
+ * 1. npm run build
+ * 2. firebase deploy
+ * 3. 
+ * 4. 
+ * 5. 
  *  */
